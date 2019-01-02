92e372ed93cc7bcfb9d8f416fab509fa0d43bf50
Samuel Martin <Samuel Martin>
2019-01-02 11:42:04 +0000

    Added a write_table() method

--- build_class.py
+++ build_class.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
 
-import pandas as pd
-import os
 
 
 class HtmlTable:
@@ -13,6 +11,8 @@ class HtmlTable:
         self.width = str(width)
         self.bg_color = str(bg_color)
         self.alt_bg_color = str(bg_color)
+        self.saveas = 'html_table.html'
+        self.page = []
 
     def table_header(self):
         """Returns the html code for a table header, with title."""
@@ -22,6 +22,7 @@ class HtmlTable:
                 + self.bg_color \
                 + '; caption-side: top"><h3>' \
                 + self.title + '</h3></caption>'
+        self.page.append(header)
         return header
 
     def table_columns(self):
@@ -30,6 +31,7 @@ class HtmlTable:
             <col style="background-color: ' + self.alt_bg_color + '">\
             <col style="background-color: ' + self.bg_color + '">\
                 </colgroup>'
+        self.page.append(columns)
         return columns
 
     def column_headings(self, td1='One', td2='Two', td3='Three'):
@@ -38,6 +40,7 @@ class HtmlTable:
                         <th scope="col">' + td2 + '</th>\
                         <th scope="col">' + td3 + '</th>\
                     </tr>'
+        self.page.append(col_head)
         return col_head
 
     def add_row(self, entry1='entry', entry2='entry', entry3='entry'):
@@ -46,10 +49,21 @@ class HtmlTable:
                     <td>' + entry2 + '</td>\
                     <td>' + entry3 + '</td>\
                 </tr>'
+        self.page.append(row)
         return row
 
+    def write_page(self):
+        full_page = ''.join(self.page)
+        with open(self.saveas, 'w') as f:
+            f.write(full_page)
 
-def main():
+
+def test_HtmlTable():
+    """Builds a dummy table using all the class functionality, and opens
+    the table in a browser to test.
+    The 'os.system()' call should be adjusted for your own OS and browser."""
+    import os
+
     page = HtmlTable()
     header = page.table_header()
     page.alt_bg_color = "rgb(235, 180, 33"
@@ -58,9 +72,8 @@ def main():
     row1 = page.add_row()
     row2 = page.add_row()
     row3 = page.add_row()
-    with open('deleteme.html', 'w') as f:
-        f.write(header + columns + col_head + row1 + row2 + row3)
+    page.write_page()
     os.system('epiphany deleteme.html')
 
 if __name__ == '__main__':
-    main()
+    test_HtmlTable()
